308ef503b130596cc1f3f5ec916b81ca22dec2fc
Ryan Buckley <ryan@brownthwaite.com>
2020-05-04 13:41:15 +0100

    Final

---
+++ Tests/CommonTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CommonTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CommonTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac5af9e9-3fea-4d14-805d-052a83f1992a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/CommonTest/StringHandlerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonTest
+{
+    [TestClass]
+    public class StringHandlerTest
+    {
+        [TestMethod]
+        public void StringerHandlerTest()
+        {
+            var source = "SonicScrewdriver";
+            var expected = "Sonic Screwdriver";
+
+            var actual = source.InsertSpaces();
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void IinsertSpacesWithExistngSpacesTest()
+        {
+            var source = "Sonic Screwdriver";
+            var expected = "Sonic Screwdriver";
+
+            var actual = source.InsertSpaces();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
